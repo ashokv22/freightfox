9fe09423f179aee03767519ce92da1f4558d072e
ashok.v@sen-sei.in <ashok.v@sen-sei.in>
2024-04-28 17:48:33 +0530

    What's new: 2. Feign Integration for OpenWeather 3rd party APIs

--- src/main/java/com/freightfox/demo/Application.java
+++ src/main/java/com/freightfox/demo/Application.java
@@ -1,12 +1,17 @@
 package com.freightfox.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"com.freightfox.demo.*"})
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class Application {
 
 	@Bean
